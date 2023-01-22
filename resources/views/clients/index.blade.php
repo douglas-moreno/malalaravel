b67dcd8c569ada0a9d5b845600c2ab3b082b8e01
Douglas Moreno <douglas_moreno@yahoo.com.br>
2023-01-22 20:56:11 -0300

    Navbar added

--- resources/views/clients/index.blade.php
+++ resources/views/clients/index.blade.php
@@ -4,27 +4,28 @@
 
 @section('content')
     <h1 class="text-center text-2xl p-4">Lista de Eleitores</h1>
-
-    <table class="table-auto border-separate border border-slate-500">
-        <thead>
-            <tr>
-                <th>Nome</th>
-                <th>Aniversario</th>
-                <th>Endereco</th>
-                <th>Numero</th>
-                <th>Bairro</th>
-                <th>Acao</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($clients as $client)
-                <tr><td>{{$client->name}}</td>
-                <td>{{$client->birthday}}</td>
-                <td>{{$client->street}}</td>
-                <td>{{$client->number}}</td>
-                <td>{{$client->neighborhood}}</td>
-                <td><a href="{{ route('client.show', $client->id) }}">Detalhes</a></td></tr>
-            @endforeach                
-        </tbody>
-    </table>
+    <div class="flex items-center justify-between flex-wrap">
+        <table class="table-auto border">
+            <thead>
+                <tr>
+                    <th class="py-2 border border-slate-800">Nome</th>
+                    <th class="py-2 border border-slate-800">Aniversario</th>
+                    <th class="py-2 border border-slate-800">Endereco</th>
+                    <th class="py-2 border border-slate-800">Numero</th>
+                    <th class="py-2 border border-slate-800">Bairro</th>
+                    <th class="py-2 border border-slate-800">Acao</th>
+                </tr>
+            </thead>
+            <tbody >
+                @foreach ($clients as $client)
+                    <tr><td class="border border-slate-500">{{$client->name}}</td>
+                    <td class="border border-slate-500">{{$client->birthday}}</td>
+                    <td class="border border-slate-500">{{$client->street}}</td>
+                    <td class="border border-slate-500">{{$client->number}}</td>
+                    <td class="border border-slate-500">{{$client->neighborhood}}</td>
+                    <td class="border border-slate-500"><a href="{{ route('client.show', $client->id) }}">Detalhes</a></td></tr>
+                @endforeach                
+            </tbody>
+        </table>
+    </div>
 @endsection
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -4,11 +4,39 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <script src="https://cdn.tailwindcss.com"></script>
+    @vite('resources/css/app.css')
     
     <title>@yield('title')</title>
 </head>
 <body class="bg-gray-100">
+    <div class="container mx-auto">
+        <nav class="flex items-center justify-between flex-wrap bg-indigo-500 p-6">
+            <div class="flex items-center flex-shrink-0 text-white mr-6">
+            <span class="font-semibold text-xl tracking-tight">Mala Web</span>
+            </div>
+            <div class="block lg:hidden">
+            <button class="flex items-center px-3 py-2 border rounded text-indigo-200 border-indigo-400 hover:text-white hover:border-white">
+                <svg class="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
+            </button>
+            </div>
+            <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+            <div class="text-sm lg:flex-grow">
+                <a href="{{ route('client.create') }}" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
+                Novo Eleitor
+                </a>
+                <a href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
+                Nova Pesquisa
+                </a>
+                <a href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white">
+                Imprimir
+                </a>
+            </div>
+            <div>
+                <a href="#" class="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0">Admin</a>
+            </div>
+            </div>
+        </nav>
+    </div>
     <div class="container mx-auto">
         @yield('content')
     </div>
